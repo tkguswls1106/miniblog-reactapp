6442614b25f1853d64d3f1656404308cce1786d7
sahyunjin <tkguswls1106@naver.com>
2023-01-14 22:48:40 +0900

    사용자가 볼 첫메인화면인 MainPage.jsx 생성하고 코드 작성함. 그리고 이 파일과 PostList.jsx 파일이 연관된 이벤트함수에 대한 보충설명을 주석으로 적어두었음.

---
+++ src/component/page/MainPage.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import PostList from "../list/PostList";
+import Button from "../ui/Button";
+import data from '../../data.json';
+
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+function MainPage(props) {
+    const {} = props;
+
+    const navigate = useNavigate();
+
+    return (
+        <Wrapper>
+            <Container>
+                <Button
+                    title="글 작성하기"
+                    onClick={() => {  // 나중에 onClick 함수 호출할때, 파라미터없이 호출 가능하게 만듦.
+                        navigate("/post-write");
+                    }}
+                />
+
+                <PostList
+                    posts={data}
+                    onClickItem={(item) => {  // 나중에 onClickItem 함수 호출할때, 파라미터를 하나 갖고서 호출하게 만듦.
+                        navigate(`/post/${item.id}`);
+                    }}
+                />
+            </Container>
+        </Wrapper>
+    );
+}
+
+export default MainPage;
