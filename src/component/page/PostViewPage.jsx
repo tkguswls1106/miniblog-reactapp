cf3ac943f7b9ae273054ea20383ef27d01ff9500
sahyunjin <tkguswls1106@naver.com>
2023-01-14 23:40:18 +0900

    사용자가 선택한 글(+해당 글의 댓글들)을 보여주는 페이지인 PostViewPage.jsx 생성하고 코드 작성함. 추가설명은 주석으로 작성하였음.

---
+++ src/component/page/PostViewPage.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
+import CommentList from "../list/CommentList";
+import TextInput from "../ui/TextInput";
+import Button from "../ui/Button";
+import data from "../../data.json";
+
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+const PostContainer = styled.div`
+    padding: 8px 16px;  // 위쪽 아래쪽 padding 8px, 왼쪽 오른쪽 padding 16px
+    border: 1px solid grey;
+    border-radius: 8px;
+`;
+
+const TitleText = styled.p`
+    font-size: 28px;
+    font-weight: 500;
+`;
+
+const ContentText = styled.p`
+    font-size: 20px;
+    line-height: 32px;
+    white-space: pre-wrap;
+`;
+
+const CommentLabel = styled.p`
+    font-size: 16px;
+    font-weight: 500;
+`;
+
+function PostViewPage(props) {
+    const navigate = useNavigate();
+    const { postId } = useParams();
+
+    const post = data.find((item) => {  // 먼저 props로 전달받은 글의 id를 통해서, 전체 data데이터에서 해당 id와 일치하는 글을 찾아서 불러와, 변수 post에 할당한다.
+        return item.id == postId;
+    });
+
+    const [comment, setComment] = useState("");
+
+    return (
+        <Wrapper>
+            <Container>
+                <Button
+                    title="뒤로 가기"
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                />
+                <PostContainer>
+                    <TitleText>{post.title}</TitleText>
+                    <ContentText>{post.content}</ContentText>
+                </PostContainer>
+
+                <CommentLabel>댓글</CommentLabel>
+                <CommentList comments={post.comments} />
+
+                <TextInput
+                    height={40}
+                    value={comment}
+                    onChange={(event) => {
+                        setComment(event.target.value);
+                    }}
+                />
+                <Button
+                    title="댓글 작성하기"
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                />
+            </Container>
+        </Wrapper>
+    );
+}
+
+export default PostViewPage;
